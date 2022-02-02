0907be324b588f94a0fac806fc4c34ee36b437a2
Louis Pearson <louispearson@librem.one>
2022-02-02 15:49:23 -0700

    Complete stateful list lecture

--- code/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ code/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -13,7 +13,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={props.clickHandler}>Change Title</button>
     </Card>
   );
 };
