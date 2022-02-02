7c321ac02fe2d1750818eef8eaa40dbd25d90954
Louis Pearson <louispearson@librem.one>
2022-02-01 18:12:21 -0700

    Organize component files

--- code/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ code/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 // className instead of class, because this gets compiled down to javascript
 function ExpenseItem(props) {
