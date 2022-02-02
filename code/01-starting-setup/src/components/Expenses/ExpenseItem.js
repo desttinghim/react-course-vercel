9b1f1bc104a6028f4a86c459b603894b888c206a
Louis Pearson <louispearson@librem.one>
2022-02-01 20:01:21 -0700

    Complete control and state lectures
    An uncontrolled component does not recieve state. A controlled component
    does. A stateless (or dumb or presentational) component does not store
    state, a stateful one does. Usually there are more stateless components
    in a React project than stateful ones.

--- code/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ code/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -6,25 +6,14 @@ import Card from "../UI/Card";
 
 // className instead of class, because this gets compiled down to javascript
 const ExpenseItem = (props) => {
-  // function clickHandler()
-  // State is per component instance
-  const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
-
-  // End event listeners with Handler
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={props.clickHandler}>Change Title</button>
     </Card>
   );
 };
