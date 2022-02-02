02120a3823d452299008e98d20604d6ebed949ea
Louis Pearson <louispearson@librem.one>
2022-02-01 19:26:48 -0700

    Complete child-to-parent communication

--- code/01-starting-setup/src/App.js
+++ code/01-starting-setup/src/App.js
@@ -26,6 +26,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   "div",
   //   {},
@@ -36,7 +41,7 @@ const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
