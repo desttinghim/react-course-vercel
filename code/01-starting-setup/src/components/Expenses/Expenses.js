3005208bd1f2caf0630276d164dc3885de91e3d5
Louis Pearson <louispearson@librem.one>
2022-02-01 19:51:12 -0700

    Assignment 2: Events & State

--- code/01-starting-setup/src/components/Expenses/Expenses.js
+++ code/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const onFilterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterChange={onFilterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
