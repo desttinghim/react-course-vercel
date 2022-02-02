ebd454a62731370bffd7605813aaa8a0cebe2115
Louis Pearson <louispearson@librem.one>
2022-02-01 18:36:34 -0700

    Complete working with state lecture

--- code/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ code/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
@@ -6,13 +6,23 @@ import Card from "../UI/Card";
 
 // className instead of class, because this gets compiled down to javascript
 const ExpenseItem = (props) => {
+  // function clickHandler()
+  const [title, setTitle] = useState(props.title);
+
+  // End event listeners with Handler
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
