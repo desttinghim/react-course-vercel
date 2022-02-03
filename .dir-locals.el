32161700085a2137f6c1dd0e667deda2ada1adad
Louis Pearson <louispearson@librem.one>
2022-02-02 20:54:29 -0700

    Complete working with breakpoints

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-((rjsx-mode . ((projectile-other-file-alist . ("js" . ("module.css")))))
- (css-mode . ((projectile-other-file-alist . ("module.css" . ("js"))))))
+((nil . ((projectile-other-file-alist . (("js" . ("module.css")
+                                          ("module.css" . ("js"))))))))
