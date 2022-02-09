bcb05f8f2d41012d6eda1f979900ee1ab23b9d9f
Louis Pearson <louispearson@librem.one>
2022-02-09 14:36:57 -0700

    Log entered value

--- set3/16-forms-user-input/src/components/SimpleInput.js
+++ set3/16-forms-user-input/src/components/SimpleInput.js
@@ -1,9 +1,38 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // NOTE: NOT IDEAL, DON'T MANIPULATE THE DOM.
+    // Allow React to do all dom manipulations
+    // nameInputRef.current.value = '';
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
