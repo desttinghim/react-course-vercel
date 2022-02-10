e9aa3828a6bf6c1fd36780154f4e43684a2a7bb3
Louis Pearson <louispearson@librem.one>
2022-02-10 13:20:35 -0700

    Implement login/logout

--- set3/18-redux/src/App.js
+++ set3/18-redux/src/App.js
@@ -1,9 +1,24 @@
-import Counter from './components/Counter';
+import { Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+
+// import { authActions } from "./store/index";
 
 function App() {
+  // const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
