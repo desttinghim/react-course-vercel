38655b1f0dc26930be9d9a16fa60ea9c39b7d544
Louis Pearson <louispearson@librem.one>
2022-02-09 19:48:38 -0700

    Get checkout working I guess

--- set3/17-reactmeal-http/src/components/Cart/Order.js
+++ set3/17-reactmeal-http/src/components/Cart/Order.js
@@ -3,31 +3,71 @@ import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
-import OrderForm from "./OrderForm";
+import useInput from "../../hooks/use-input";
+import FormInput from "../UI/FormInput";
 
 const Order = (props) => {
+  // Form
+  const nameInput = useInput((value) => value.trim() !== "");
+  const addressInput = useInput((value) => value.trim() !== "");
+
+  const formIsValid = nameInput.isValid && addressInput.isValid;
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) return;
+
+    console.log(nameInput.value, addressInput.value);
+
+    nameInput.reset();
+    addressInput.reset();
+  };
+
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
-  const hasItems = cartCtx.items.length > 0;
+
+  const orderForm = (
+    <form onSubmit={submitHandler}>
+      <FormInput
+        label="Full Name"
+        errorMessage="Name cannot be empty."
+        inputHook={nameInput}
+        input={{ id: "name" }}
+      />
+      <FormInput
+        label="Addres"
+        errorMessage="Address cannot be empty."
+        inputHook={addressInput}
+        input={{ id: "address" }}
+      />
+      <div className={classes.actions}>
+        <button
+          type="button"
+          className={classes["button--alt"]}
+          onClick={props.onClose}
+        >
+          Close
+        </button>
+        <button
+          type="submit"
+          className={classes.button}
+          onClick={props.onOrder}
+        >
+          Confirm
+        </button>
+      </div>
+    </form>
+  );
 
   return (
     <Modal onClose={props.onClose}>
-      <OrderForm />
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-        {hasItems && (
-          <button className={classes.button} onClick={props.onOrder}>
-            Confirm
-          </button>
-        )}
-      </div>
+      {orderForm}
     </Modal>
   );
 };
--- set3/17-reactmeal-http/src/components/Cart/OrderForm.js
+++ set3/17-reactmeal-http/src/components/Cart/OrderForm.js
@@ -3,33 +3,35 @@ import FormInput from "../UI/FormInput";
 
 const OrderForm = (props) => {
   const nameInput = useInput((value) => value.trim() !== "");
+  const addressInput = useInput((value) => value.trim() !== "");
 
-  const formIsValid = nameInput.isValid;
+  const formIsValid = nameInput.isValid && addressInput.isValid;
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     if (!formIsValid) return;
+
+    console.log(nameInput.value, addressInput.value);
+
+    nameInput.reset();
+    addressInput.reset();
   };
 
   return (
     <form onSubmit={submitHandler}>
       <FormInput
         label="Full Name"
+        errorMessage="Name cannot be empty."
         inputHook={nameInput}
         input={{ id: "name" }}
       />
-      <div>
-        <label htmlFor="name">Full Name</label>
-        <input
-          name="name"
-          type="text"
-          value={nameInput.value}
-          onChange={nameInput.valueChangeHandler}
-          onBlur={nameInput.inputBlurHandler}
-        />
-        {nameInput.hasError && <p>Name cannot be empty.</p>}
-      </div>
+      <FormInput
+        label="Addres"
+        errorMessage="Address cannot be empty."
+        inputHook={addressInput}
+        input={{ id: "address" }}
+      />
     </form>
   );
 };
