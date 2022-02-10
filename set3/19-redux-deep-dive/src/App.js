5d08ae1c868b86bca35f947e082a64e7d5b74ca7
Louis Pearson <louispearson@librem.one>
2022-02-10 16:48:36 -0700

    Send cart data to server

--- set3/19-redux-deep-dive/src/App.js
+++ set3/19-redux-deep-dive/src/App.js
@@ -1,17 +1,82 @@
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+
+import { uiActions } from "./store/ui";
+
+let isInitial = true;
 
 function App() {
-  const ui = useSelector((store) => store.ui);
+  const dispatch = useDispatch();
+  const ui = useSelector((state) => state.ui);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://react-http-3d4aa-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      console.error(error);
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: error.message,
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {ui.cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {ui.cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
