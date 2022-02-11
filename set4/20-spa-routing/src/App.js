8efc6a2349fc59ae89235523bd9aad078b813dab
Louis Pearson <louispearson@librem.one>
2022-02-11 12:47:30 -0700

    Dynamic routing

--- set4/20-spa-routing/src/App.js
+++ set4/20-spa-routing/src/App.js
@@ -1,23 +1,33 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import MainHeader from "./components/MainHeader";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Product Component
+// our-domain.com/product-detail/<any value>
