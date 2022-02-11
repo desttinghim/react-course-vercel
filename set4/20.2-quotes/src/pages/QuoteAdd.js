51363fddfdc4a6b86abe744937db93f22b5882b2
Louis Pearson <louispearson@librem.one>
2022-02-11 14:34:50 -0700

    useHistory and prompt

--- set4/20.2-quotes/src/pages/QuoteAdd.js
+++ set4/20.2-quotes/src/pages/QuoteAdd.js
@@ -1,8 +1,14 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from "../components/quotes/QuoteForm"
 
 const QuoteAdd = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push('/quotes');
   };
 
   return (
