25c02ed63080518a64e8538bb2204d7aef608e4b
Louis Pearson <louispearson@librem.one>
2022-02-11 13:47:54 -0700

    Add nested route

--- set4/20.2-quotes/src/pages/QuoteDetail.js
+++ set4/20.2-quotes/src/pages/QuoteDetail.js
@@ -1,6 +1,19 @@
+import { Fragment } from 'react';
+import { Route, useParams } from 'react-router-dom';
+
+import Comments from "../components/comments/Comments";
+
 const QuoteDetail = () => {
+  const params = useParams();
+
   return (
-    <h1>Quote Detail</h1>
+    <Fragment>
+      <h1>Quote Detail</h1>
+      <p> {params.quoteId} </p>
+      <Route path="/quotes/:quoteId/comments">
+        <Comments />
+      </Route>
+    </Fragment>
   );
 };
 
