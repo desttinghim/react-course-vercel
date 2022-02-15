98f7e5e76c1974941b0c4f8f3d51eef67ae1dc0f
Louis Pearson <louispearson@librem.one>
2022-02-14 20:13:48 -0700

    Statically generate meetup details page

--- set4/23-bigger-nextjs/pages/[meetupId]/index.js
+++ set4/23-bigger-nextjs/pages/[meetupId]/index.js
@@ -1,7 +1,7 @@
 // our-domain.com/[meetupId]
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
       image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/320px-Stadtbild_M%C3%BCnchen.jpg"
@@ -10,6 +10,44 @@ const MeetupDetails = () => {
       description="This is a First Meetup"
     />
   );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  // fetch data for a single meetup
+
+  const meetupId = context.params;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/320px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a First Meetup",
+      },
+    },
+  };
 };
 
 export default MeetupDetails;
