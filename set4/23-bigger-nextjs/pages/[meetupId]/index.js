6aea8986efe8eaab18f6bc40031a4b8e29a356e7
Louis Pearson <louispearson@librem.one>
2022-02-14 19:49:32 -0700

    Add MeetupDetail

--- set4/23-bigger-nextjs/pages/[meetupId]/index.js
+++ set4/23-bigger-nextjs/pages/[meetupId]/index.js
@@ -1,1 +1,15 @@
 // our-domain.com/[meetupId]
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/320px-Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a First Meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
