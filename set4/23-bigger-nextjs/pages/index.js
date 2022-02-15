775c5a22f3ec91f362597893db97b671b3de7ee8
Louis Pearson <louispearson@librem.one>
2022-02-14 19:31:22 -0700

    Add pages

---
+++ set4/23-bigger-nextjs/pages/index.js
@@ -0,0 +1,27 @@
+// our-domain.com/
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/320px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "m2",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/320px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some address 5, 12345 Some City",
+    description: "This is a first meetup!",
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
